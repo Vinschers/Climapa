d28b3c44b8725c301fdf2b3e7b5b73124a6379c9
Scherer <vincentin.py@gmail.com>
2022-10-15 00:31:39 -0300

    Add all countries

--- bd/inserts/regiao.py
+++ bd/inserts/regiao.py
@@ -2,21 +2,69 @@ import json
 
 import requests
 
-regioes = []
+regions = [
+    "",
+    "América",
+    "Ásia",
+    "Europa",
+    "Oceania",
+    "África",
+    "Europa meridional  (Sul da Europa)",
+    "Ásia ocidental  (Oeste da Ásia)",
+    "Ásia meridional (Sul da Ásia)",
+    "América Latina e Caribe",
+    "Europa ocidental (Oeste da Europa)",
+    "Austrália e Nova Zelândia",
+    "Europa oriental (Leste da Europa)",
+    "Sudeste da Ásia",
+    "Ásia oriental (Leste da Ásia)",
+    "Europa setentrional (Norte da Europa)",
+    "Melanésia",
+    "Micronésia",
+    "Ásia central",
+    "Polinésia",
+    "América do Norte",
+    "Caribe",
+    "América central",
+    "América do sul",
+    "África central",
+    "África setentrional (Norte da África)",
+    "África ocidental (Oeste da África)",
+    "África oriental (Leste da África)",
+    "África meridional (Sul da África)",
+]
+
+countries = []
+
+
+def get_parent(local: dict) -> str:
+    ri = regions.index(local["regiao-intermediaria"]["nome"]) if local["regiao-intermediaria"] else 0
+    if ri > 0:
+        return str(ri)
+
+    sr = regions.index(local["sub-regiao"]["nome"]) if local["sub-regiao"] else 0
+    if sr > 0:
+        return str(sr)
+
+    r = regions.index(local["regiao"]["nome"]) if local["regiao"] else 0
+    if r > 0:
+        return str(r)
+
+    return "NULL"
+
 
 def extract_info(pais: dict) -> dict:
-    sr = pais["localizacao"]["regiao-intermediaria"]["nome"] if pais["localizacao"]["regiao-intermediaria"] else ""
-
-    if sr not in regioes:
-        regioes.append(sr)
     return {
         "nome": pais["nome"]["abreviado"],
-        "tamanho": pais["area"]["total"]
+        "tamanho": pais["area"]["total"],
+        "tipo": 4,
+        "pai": get_parent(pais["localizacao"]),
+        "artigo": "a",
     }
 
 
 def get_sql(info: dict) -> str:
-    return ""
+    return f"INSERT INTO Regiao(nome, tamanho, id_tipo_regiao, id_regiao_pai, artigo) VALUES ('{info['nome']}', {info['tamanho']}, {info['tipo']}, {info['pai']}, '{info['artigo']}');"
 
 
 if __name__ == "__main__":
@@ -26,9 +74,11 @@ if __name__ == "__main__":
 
     for pais in all:
         info = extract_info(pais)
-        sql_pais = get_sql(info)
 
-        sql += f"{sql_pais}\n"
+        if info not in countries:
+            countries.append(info)
 
-    # print(sql)
-    print(regioes)
+    for country in countries:
+        sql += f"{get_sql(country)}\n"
+
+    print(sql)
