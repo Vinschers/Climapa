d4ba5319e530ac8896b84321deb042260ab75c03
Luan Fazolin <87768066+lufazol@users.noreply.github.com>
2022-10-15 02:19:19 -0300

    Add Editor

---
+++ bd/inserts/Editor.sql
@@ -0,0 +1,10 @@
+INSERT INTO Editor(nome, formacao, id_regiao) VALUES('Roberto Martins','geologia','164');
+INSERT INTO Editor(nome, formacao, id_regiao) VALUES('Mariana Suzuki','engenharia quimica','197');
+INSERT INTO Editor(nome, formacao, id_regiao) VALUES('Beatriz Cunha','outro','82');
+INSERT INTO Editor(nome, formacao, id_regiao) VALUES('Mariana Dolabella','outro','22');
+INSERT INTO Editor(nome, formacao, id_regiao) VALUES('Patricia Pereira','oceanografia','89');
+INSERT INTO Editor(nome, formacao, id_regiao) VALUES('Beatriz Ferreira','estatistica','18');
+INSERT INTO Editor(nome, formacao, id_regiao) VALUES('Carlos Pereira','fisica','163');
+INSERT INTO Editor(nome, formacao, id_regiao) VALUES('Ana Cunha','geologia','177');
+INSERT INTO Editor(nome, formacao, id_regiao) VALUES('Francisco Guerra','geografia','59');
+INSERT INTO Editor(nome, formacao, id_regiao) VALUES('Beatriz Martins','fisica','180');
