297a1dc14a1ea9dae254c1b4c45171dbe10303e9
João Victor Pátaro <joaovictor.pataro@gmail.com>
2022-10-14 21:32:31 -0300

    Add files via upload
    Script for inserting data in table TipoEnergia

---
+++ bd/inserts/TipoEnergia.sql
@@ -0,0 +1,6 @@
+INSERT inTO TipoEnergia VALUES('Nuclear', 'Energia gerada a partir de usinas nucleares')
+INSERT inTO TipoEnergia VALUES('Eólica',  'Energia gerada a partir dos ventos')
+INSERT inTO TipoEnergia VALUES('Solar', 'Energia gerada a partir de placas solares que capturam a luz do sol')
+INSERT inTO TipoEnergia VALUES('Hidrelétrica', 'Energia gerada a partir das correntes da água')
+INSERT inTO TipoEnergia VALUES('Biomassa', 'Energia gerada a partir da queima de matérias orgânicas')
+INSERT inTO TipoEnergia VALUES('Combustíveis Fósseis', 'Energia gerada a partir da queima de materiais derivados de petróleo')
